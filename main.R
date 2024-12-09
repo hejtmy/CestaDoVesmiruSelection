54cbf53b730eb46b1095fdf0e7a74b01d6dab509
Lukáš Hejtmánek <hejtmy@gmail.com>
2024-12-09 02:28:38 +0100

    Adds new tests of existing tests

--- main.R
+++ main.R
@@ -22,12 +22,11 @@ con <- dbConnect(
 query <- 'SELECT * FROM "TestResults" LIMIT 50'
 
 results <- dbGetQuery(con, query)
-txt <- results$test_results[40]
-participant <- results$id[40]
+participant <- results$id[45]
+txt <- results$test_results[47]
 arr <- fromJSON(txt)
 df <- as.data.frame(fromJSON(arr[[1]]))
-View(df)
-
+df
 # Send query
 query <- dbSendQuery(con, "SELECT * FROM TestResults")
 
@@ -41,12 +40,47 @@ while (!dbHasCompleted(query)) {
 dbClearResult(query)
 
 ## GONOGO
+txt <- results$test_results[40]
+arr <- fromJSON(txt)
 df <- as.data.frame(fromJSON(arr[[1]]))
-colnames(df)
 get_browser_information(df)
 
 df_gng <- gonogo_create_trial_table(df)
+gonogo_analyze_performance(df_gng)
+
+## NBACK
+txt <- results$test_results[47]
+arr <- fromJSON(txt)
+df <- as.data.frame(fromJSON(arr[[1]]))
+colnames(df)
+View(df)
+
+df <- select(df, trial_index, time_elapsed, rt,
+               response, block_number, image)
 
 
+## EMOTIONS
+txt <- results$test_results[48]
+arr <- fromJSON(txt)
+df <- as.data.frame(fromJSON(arr[[1]]))
+head(df)
 
-View(df_gng)
+
+## Spatial
+txt <- results$test_results[42]
+arr <- fromJSON(txt)
+df <- as.data.frame(fromJSON(arr[[1]]))
+View(df)
+
+
+#  from the following image tag extract the final name of hte image and the rotation from the rotate_X class
+#  <img src="https://unyavwofnmwihnpvkjji.supabase.co/storage/v1/object/public/test-stimuli/spatialcognition/map_right_1_135.png" class="max-w-none rotate_0" width="512" height="512" alt="Mapa">
+extract_image_info <- function(df) {
+  df <- df %>%
+    mutate(
+      image = gsub("https://unyavwofnmwihnpvkjji.supabase.co/storage/v1/object/public/test-stimuli/spatialcognition/", "", image),
+      image = gsub(".png", "", image),
+      rotation = as.numeric(gsub("rotate_", "", gsub(".*rotate_", "", image)))
+    )
+  return(df)
+}
